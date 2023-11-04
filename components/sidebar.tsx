2af31837d4491f9bc355ad32929e7d3cf896992a
ychen463 <gisellec2@163.com>
2023-11-04 03:12:47 -0400

    api limit counter UI done

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,7 +7,7 @@ import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, Video
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
-// import { FreeCounter } from "@/components/free-counter";
+import { FreeCounter } from "@/components/free-counter";
 
 const poppins = Montserrat({ weight: '600', subsets: ['latin'] });
 
@@ -93,10 +93,7 @@ export const Sidebar = ({
                     ))}
                 </div>
             </div>
-            {/* <FreeCounter
-                apiLimitCount={apiLimitCount}
-                isPro={isPro}
-            /> */}
+            <FreeCounter apiLimitCount={apiLimitCount} isPro={isPro} />
         </div>
     );
 };
